c7c57df096742514ba480995753e850d9e104a26
Riley Broderick <rjb3977@rit.edu>
2020-02-28 19:22:41 -0500

    initial commit

---
+++ Lattice.cs
@@ -0,0 +1,243 @@
+using System;
+using System.Collections.Generic;
+using Extreme.Mathematics;
+using Microsoft.Z3;
+using System.Linq;
+
+namespace Lattice
+{
+    public static class Lattice
+    {
+        private static bool GetBit(this ulong x, int n)
+        {
+            return ((x >> n) & 1) != 0;
+        }
+
+        public static List<Vector<BigInteger>> Enumerate(int dimensions, BigRational lower, BigRational upper, Matrix<BigRational> basis)
+        {
+            var basisInverse = basis.GetInverse();
+            var vertices = 1uL << dimensions;
+            var generators = new List<ValueTuple<Vector<BigInteger>, Vector<BigInteger>>>();
+
+            for (var i = 0uL; i < vertices; ++i)
+            {
+                var vertexEntries = new BigRational[dimensions];
+                var neighbors = new List<Vector<BigInteger>>();
+
+                for (var j = 0; j < dimensions; ++j)
+                {
+                    vertexEntries[j] = i.GetBit(j) ? upper : lower;
+                }
+
+                var vertex = basisInverse * Vector.Create(vertexEntries);
+
+                for (var j = 0; j < dimensions; ++j)
+                {
+                    var component = i.GetBit(j) ? lower - upper : upper - lower;
+                    var neighbor = basisInverse * Vector.Create(dimensions, k => k == j ? component : 0);
+
+                    var gcd = BigInteger.Zero;
+                    var lcm = BigInteger.One;
+
+                    for (var k = 0; k < dimensions; ++k)
+                    {
+                        gcd = BigInteger.GreatestCommonDivisor(gcd, neighbor[k].Numerator);
+                        lcm = BigInteger.LeastCommonMultiple(lcm, neighbor[k].Denominator);
+                    }
+
+                    neighbor *= new BigRational(lcm, gcd);
+                    neighbors.Add(Vector.Create(dimensions, k => neighbor[k].Numerator));
+                }
+
+                foreach (var n in neighbors)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+
+            return new List<Vector<BigInteger>>();
+        }
+
+        private static Vector<BigRational> Example = Vector.Create<BigRational>(
+            253337345868468,
+            -22694297408,
+            -185181437920550,
+            -210385418508672,
+            -50390170855715,
+            -108859710755132,
+            -69114274894019,
+            -55156597183194,
+            -116063811706968,
+            -159549169155694,
+            -17935017627050,
+            -82866953394718
+        );
+
+        public static List<Vector<BigInteger>> Enumerate2(int dimensions, BigRational lower, BigRational upper, Matrix<BigRational> basis, Vector<BigRational> offset)
+        {
+            using var context = new Context();
+            var variables = new ArithExpr[dimensions];
+            var basisInverse = basis.GetInverse();
+
+            for (var i = 0; i < dimensions; ++i)
+            {
+                variables[i] = context.MkRealConst($"x{i}");
+            }
+
+            var constraints = new BoolExpr[2 * dimensions];
+
+            for (var i = 0; i < dimensions; ++i)
+            {
+                {
+                    var face = Vector.Create(dimensions, j => lower) - offset;
+                    var normal = Vector.Create(dimensions, j => j == i ? BigRational.One : 0);
+                    constraints[2 * i + 0] = InHalfPlane(context, variables, basis, basisInverse, face, normal);
+                }
+
+                {
+                    var face = Vector.Create(dimensions, j => upper) - offset;
+                    var normal = Vector.Create(dimensions, j => j == i ? BigRational.MinusOne : 0);
+                    constraints[2 * i + 1] = InHalfPlane(context, variables, basis, basisInverse, face, normal);
+                }
+            }
+
+            // var values = new ArithExpr[dimensions];
+
+            // for (var i = 0; i < dimensions; ++i)
+            // {
+            //     values[i] = context.MkInt(Example[i].ToString());
+            // }
+
+            // foreach (var assertion in assertions)
+            // {
+            //     Console.WriteLine(assertion.Substitute(variables, values).Simplify());
+            // }
+
+            // var handle = optimize.MkMaximize(variables[0]);
+            // var status = optimize.Check();
+            // // optimize.Model.Evalu
+
+            // Console.WriteLine(status);
+            // Console.WriteLine(handle.Value);
+
+            var result = new List<Vector<BigInteger>>();
+            Solve(context, variables, constraints, new BigInteger?[dimensions], result);
+
+            return result;
+        }
+
+        private static void Solve(Context context, ArithExpr[] variables, BoolExpr[] constraints, BigInteger?[] values, List<Vector<BigInteger>> result)
+        {
+            var index = -1;
+            var (min, max) = (BigInteger.MinValue, BigInteger.MaxValue);
+
+            for (var i = 0; i < variables.Length; ++i)
+            {
+                if (values[i] == null)
+                {
+                    var (currentMin, currentMax) = GetRange(context, variables, constraints, values, i);
+
+                    if (currentMax - currentMin < max - min)
+                    {
+                        index = i;
+                        (min, max) = (currentMin, currentMax);
+                    }
+                }
+            }
+
+            if (index == -1)
+            {
+                result.Add(Vector.Create(variables.Length, i => values[i].Value));
+                // Console.WriteLine("result");
+            }
+            else
+            {
+                // Console.WriteLine($"range of x{index} is {min} -> {max}");
+
+                for (var x = min; x <= max; x += 1)
+                {
+                    // Console.WriteLine($"setting x{index} to {x}");
+
+                    values[index] = x;
+                    Solve(context, variables, constraints, values, result);
+                }
+
+                values[index] = null;
+            }
+        }
+
+        private static ValueTuple<BigInteger, BigInteger> GetRange(Context context, ArithExpr[] variables, BoolExpr[] constraints, BigInteger?[] values, int index)
+        {
+            using var optimize = context.MkOptimize();
+
+            optimize.Assert(constraints);
+
+            for (var i = 0; i < variables.Length; ++i)
+            {
+                if (values[i] != null)
+                {
+                    optimize.Assert(context.MkEq(variables[i], context.MkInt(values[i].ToString())));
+                }
+            }
+
+            optimize.Push();
+            var minHandle = optimize.MkMinimize(variables[index]);
+            optimize.Check();
+            var min = ParseBigRational(minHandle.Value.ToString());
+            optimize.Pop();
+
+            optimize.Push();
+            var maxHandle = optimize.MkMaximize(variables[index]);
+            optimize.Check();
+            var max = ParseBigRational(maxHandle.Value.ToString());
+            optimize.Pop();
+
+            return (BigRational.Ceiling(min).Numerator, BigRational.Floor(max).Numerator);
+        }
+
+        private static BigRational ParseBigRational(string x)
+        {
+            var split = x.Split("/");
+
+            if (split.Length == 1)
+            {
+                return new BigRational(BigInteger.Parse(split[0]));
+            }
+            else
+            {
+                return new BigRational(BigInteger.Parse(split[0]), BigInteger.Parse(split[1]));
+            }
+        }
+
+        private static BoolExpr InHalfPlane(Context context, ArithExpr[] variables, Matrix<BigRational> basis, Matrix<BigRational> basisInverse, Vector<BigRational> offset, Vector<BigRational> normal)
+        {
+            var rhs = Vector.DotProduct(normal, offset);
+            var lhs = normal * basis;
+            var lcm = rhs.Denominator;
+            var gcd = rhs.Numerator;
+
+            for (var i = 0; i < variables.Length; ++i)
+            {
+                lcm = BigInteger.LeastCommonMultiple(lcm, lhs[i].Denominator);
+                gcd = BigInteger.GreatestCommonDivisor(gcd, lhs[i].Numerator);
+            }
+
+            var scale = new BigRational(lcm, gcd);
+
+            rhs *= scale;
+            lhs *= scale;
+
+            var rhsExpr = context.MkInt(rhs.ToString());
+            var lhsExprs = new ArithExpr[variables.Length];
+
+            for (var i = 0; i < variables.Length; ++i)
+            {
+                lhsExprs[i] = variables[i] * context.MkInt(lhs[i].ToString());
+            }
+
+            // Console.WriteLine(context.MkAdd(lhsExprs) >= rhsExpr);
+
+            return context.MkAdd(lhsExprs) >= rhsExpr;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+// using MathNet.Numerics;
+// using MathNet.Numerics.LinearAlgebra;
+//using MathNet.Symbolics;
+
+using Extreme.Mathematics;
+
+namespace Lattice
+{
+    static class Program
+    {
+        static Program()
+        {
+            NumericsConfiguration.Providers.RegisterGenericProvider();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("usage: dotnet run input");
+                return;
+            }
+
+            var text = File.ReadAllLines(args[0]);
+            var dimensions = int.Parse(text[0]);
+            var basisEntries = new BigRational[dimensions, dimensions];
+            var offsetEntries = new BigRational[dimensions];
+            var line = text[dimensions + 2].Split(",");
+
+            for (var col = 0; col < dimensions; ++col)
+            {
+                offsetEntries[col] = BigInteger.Parse(line[col]);
+            }
+
+            for (var row = 0; row < dimensions; ++row)
+            {
+                line = text[row + 1].Split(",");
+
+                for (var col = 0; col < dimensions; ++col)
+                {
+                    basisEntries[col, row] = BigInteger.Parse(line[col]);
+                }
+            }
+
+            var upper = BigRational.Pow(2, 48);
+            var lower = upper - upper / 10;
+            var basis = Matrix.Create(basisEntries);
+            var offset = Vector.Create(offsetEntries);
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var results = Lattice.Enumerate2(dimensions, lower, upper, basis, offset);
+
+            sw.Stop();
+
+            foreach (var x in results)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine($"total:   {results.Count}");
+            Console.WriteLine($"elapsed: {sw.Elapsed}");
+        }
+    }
+}
